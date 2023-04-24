d3104185f28eedf545c0249fe80e063d8e0afc85
Mukul <Mukul>
2023-04-25 00:39:22 +0530

    april 25

---
+++ solutions/1046_last_stone_weight.cpp
@@ -0,0 +1,33 @@
+/* https://leetcode.com/problems/last-stone-weight/
+1046. Last Stone Weight
+Easy
+Priority Queue
+*/
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        if(stones.size() == 1) {
+            return stones[0];
+        }
+        for(auto i: stones) {
+            pq.push(i);
+        }
+        while(1) {
+            int st1 = pq.top();
+            pq.pop();
+            int t = st1 - pq.top();
+            pq.pop();
+            if(t) {
+                pq.push(t);
+            }
+            if(pq.empty()) {
+                return 0;
+            }
+            if(pq.size() == 1) {
+                return pq.top();
+            }
+        }
+        return 0;
+    }
+};
