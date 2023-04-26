e5109fc9c1a10fd7e627b9806fc7cf0f91e7d7b1
Mukul <Mukul>
2023-04-26 13:14:13 +0530

    26 april

---
+++ solutions/15_min_stack.cpp
@@ -0,0 +1,40 @@
+/* https://leetcode.com/problems/min-stack/
+155. Min Stack
+Medium
+Stack
+*/
+class MinStack
+{
+public:
+    stack<pair<int, int>> st;
+    MinStack()
+    {
+    }
+
+    void push(int val)
+    {
+        if (st.empty())
+        {
+            st.push({val, val});
+        }
+        else
+        {
+            st.push({val, min(val, st.top().second)});
+        }
+    }
+
+    void pop()
+    {
+        st.pop();
+    }
+
+    int top()
+    {
+        return st.top().first;
+    }
+
+    int getMin()
+    {
+        return st.top().second;
+    }
+};
