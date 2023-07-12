f1f3750bef9c7566097e1c9a8f5b2d97e0a9a874
Mukul <Mukul>
2023-07-12 12:29:44 +0530

    july 12

---
+++ solutions/2001+/2769_max_achievable_number.cpp
@@ -0,0 +1,13 @@
+/* https://leetcode.com/problems/find-the-maximum-achievable-number/
+2769. Find the Maximum Achievable Number
+Easy
+Math
+*/
+class Solution
+{
+public:
+    int theMaximumAchievableX(int num, int t)
+    {
+        return num + 2 * t;
+    }
+};
