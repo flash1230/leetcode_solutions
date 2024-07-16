cc403a96038ccf913df6329b922639e62b68f1c7
Mukul <Mukul>
2024-07-16 15:24:26 +0530

    july 16 pre mbappe

---
+++ solutions/1-500/236_lca_of_bt.cpp
@@ -0,0 +1,63 @@
+/* https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+236. Lowest Common Ancestor of a Binary Tree
+Medium
+Binary Tree DFS
+*/
+class Solution
+{
+public:
+    vector<TreeNode*> ans;
+    bool hasIt(TreeNode* root, TreeNode* target)
+    {
+        if(root == target)
+        {
+            ans.push_back(root);
+            return true;
+        }
+        if(root->left == NULL && root->right == NULL)
+            return false;
+        if(!root->left)
+        {
+            bool t = hasIt(root->right, target);
+            if(t)
+                ans.push_back(root)
+            return t;
+        }
+        if(!root->right)
+        {
+            bool t = hasIt(root->left, target);
+            if(t)
+                ans.push_back(root);
+            return t;
+        }
+        bool t = hasIt(root->left) || hasIt(root->right);
+        if(t)
+            ans.push_back(root);
+        return t;
+    }
+
+    vector<TreeNode*> path(TreeNode* root, TreeNode* p)
+    {
+        hasIt(TreeNode* root, TreeNode* p);
+        return ans;
+    }
+
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if(p == q)
+            return p;
+        vector<TreeNode*> path1 = path(root, p);
+        ans.clear();
+        vector<TreeNode*> path2 = path(root, q);
+        int i = 0;
+        int n = min(path1.size(), path2.size())
+        while(i < n)
+        {
+            if(path1[i] == path2[i])
+                i++;
+            else
+                return path1[i-1];
+        }
+        return path1[i-1];
+    }
+};
