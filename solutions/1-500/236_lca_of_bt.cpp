74c5858d186b3758c402e0ba9cfd3878101051f2
Mukul <Mukul>
2024-07-16 15:28:35 +0530

    final 256

--- solutions/1-500/236_lca_of_bt.cpp
+++ solutions/1-500/236_lca_of_bt.cpp
@@ -6,59 +6,56 @@ Binary Tree DFS
 class Solution
 {
 public:
-    vector<TreeNode*> ans;
-    bool hasIt(TreeNode* root, TreeNode* target)
+    vector<TreeNode *> ans;
+    bool hasIt(TreeNode *root, TreeNode *target)
     {
-        if(root == target)
+        if (root == target)
         {
             ans.push_back(root);
             return true;
         }
-        if(root->left == NULL && root->right == NULL)
+        if (root->left == NULL && root->right == NULL)
             return false;
-        if(!root->left)
+        if (!root->left)
         {
             bool t = hasIt(root->right, target);
-            if(t)
+            if (t)
                 ans.push_back(root);
             return t;
         }
-        if(!root->right)
+        if (!root->right)
         {
             bool t = hasIt(root->left, target);
-            if(t)
+            if (t)
                 ans.push_back(root);
             return t;
         }
         bool t = hasIt(root->left, target) || hasIt(root->right, target);
-        if(t)
+        if (t)
             ans.push_back(root);
         return t;
     }
 
-    vector<TreeNode*> path(TreeNode* root, TreeNode* p)
-    {
-        hasIt(root, p);
-        reverse(ans.begin(), ans.end());
-        return ans;
-    }
-
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
     {
-        if(p == q)
+        if (p == q)
             return p;
-        vector<TreeNode*> path1 = path(root, p);
+        hasIt(root, p);
+        reverse(ans.begin(), ans.end());
+        vector<TreeNode *> path1 = ans;
         ans.clear();
-        vector<TreeNode*> path2 = path(root, q);
+        hasIt(root, q);
+        reverse(ans.begin(), ans.end());
+        vector<TreeNode *> path2 = ans;
         int i = 0;
         int n = min(path1.size(), path2.size());
-        while(i < n)
+        while (i < n)
         {
-            if(path1[i] == path2[i])
+            if (path1[i] == path2[i])
                 i++;
             else
-                return path1[i-1];
+                return path1[i - 1];
         }
-        return path1[i-1];
+        return path1[i - 1];
     }
 };
