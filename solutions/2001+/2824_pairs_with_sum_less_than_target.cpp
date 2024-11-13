7bc3d60f2e7c726761620b891874ad999cfca45a
Mukul <Mukul>
2024-11-13 11:46:09 +0530

    nov 13

---
+++ solutions/2001+/2824_pairs_with_sum_less_than_target.cpp
@@ -0,0 +1,26 @@
+/* https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/description/
+2824. Count Pairs Whose Sum is Less than Target
+Easy
+Sorting Two Pointers
+*/
+class Solution
+{
+public:
+    int countPairs(vector<int> &nums, int target)
+    {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int left = 0, right = nums.size() - 1;
+        while (left < right)
+        {
+            if (nums[left] + nums[right] < target)
+            {
+                ans += right - left;
+                left++;
+            }
+            else
+                right--;
+        }
+        return ans;
+    }
+};
